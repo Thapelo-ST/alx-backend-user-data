b00a4df8ec4f1c03d4fa5a193a7e130ebb7bff29
Thapelo-ST <tsamkelo96@gmail.com>
2024-02-23 01:00:20 +0200

    final push

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -10,11 +10,13 @@ from user import Base, User
 import bcrypt
 from db import DB
 
-def _hash_password(password: str) ->  bytes:
+
+def _hash_password(password: str) -> bytes:
     """Hashes the provided password using SHA-256"""
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
 
 def _generate_uuid() -> str:
     """generates a UUID for a user"""
@@ -26,8 +28,8 @@ class Auth:
     """
 
     def __init__(self):
+        """initialise the class"""
         self._db = DB()
-        
 
     def register_user(self, email: str, password: str) -> User:
         """Registers a new user and returns a User object"""
@@ -35,7 +37,7 @@ class Auth:
             self._db.find_user_by(email=email)
         except NoResultFound:
             hashed_password = _hash_password(password)
-            return self._db.add_user(email=email,\
+            return self._db.add_user(email=email,
                                      hashed_password=hashed_password)
 
         raise ValueError("User {} already exists".format(email))
@@ -63,3 +65,46 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User or None:
+        """
+        Returns the corresponding User for a given session ID,
+        or None if not found.
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Updates the corresponding user's session ID to None."""
+        self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates and returns a reset password token for the user."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError(f"User with email {email} not found")
+
+        reset_token = str(uuid4())
+
+        self._db.update_user(user.id, reset_token=reset_token)
+
+        return reset_token
+
+    def update_password(self, reset_token: str, new_password: str) -> None:
+        """Update user's password using reset_token."""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError("Invalid reset token")
+
+        hashed_password = _hash_password(new_password)
+
+        self._db.update_user(user.id,
+                             hashed_password=hashed_password, reset_token=None)
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,33 +1,98 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
+"""main module"""
+import requests
 
-from db import DB
-from user import User
+BASE_URL = "http://localhost:5000"
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
 
-my_db = DB()
 
-# Test Case 1: Adding a valid user
-user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-assert user_1.id is not None, "User ID should not be None"
-print("\nTest Case 1: User added successfully - ID: \n", user_1.id)
+def register_user(email: str, password: str) -> None:
+    """Registers a new user."""
+    url = f"{BASE_URL}/users"
+    data = {"email": email, "password": password}
+    response = requests.post(url, data=data)
 
-# Test Case 2: Adding another valid user
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-assert user_2.id is not None, "User ID should not be None"
-print("\nTest Case 2: Another user added successfully - ID: \n", user_2.id)
+    try:
+        assert response.status_code == 200
+        print(f"User registration successful for email: {email}")
+    except AssertionError:
+        print(f"User registration failed. Status Code: {response.status_code}\
+              , Response: {response.text}")
 
-# Test Case 3: Adding a user with an empty email (should raise IntegrityError)
-try:
-    invalid_user = my_db.add_user("", "InvalidHashedPwd")
-    print("\nTest Case 3: Unexpected - Empty email should raise IntegrityError\n")
-except Exception as e:
-    print("\nTest Case 3: Expected -\n", e)
 
-# Test Case 4: Adding a user with an empty password (should raise IntegrityError)
-try:
-    invalid_user = my_db.add_user("invalid@test.com", "")
-    print("\nTest Case 4: Unexpected - Empty password should raise IntegrityError\n")
-except Exception as e:
-    print("\nTest Case 4: Expected -\n", e)
+def log_in_wrong_password(email: str, password: str) -> None:
+    """testing password failure"""
+    url = "{}/sessions".format(BASE_URL)
+    data = {"email": email, "password": password}
+    response = requests.post(url, data=data)
+    assert response.status_code == 401
+
+
+def log_in(email: str, password: str) -> str:
+    """testing login"""
+    url = "{}/sessions".format(BASE_URL)
+    data = {"email": email, "password": password}
+    response = requests.post(url, data=data)
+    assert response.status_code == 200
+    return response.cookies.get("session_id")
+
+
+def profile_unlogged() -> None:
+    """testing profile log off"""
+    url = "{}/profile".format(BASE_URL)
+    response = requests.get(url)
+    assert response.status_code == 403
+
+
+def profile_logged(session_id: str) -> None:
+    """testing profile log in"""
+    url = "{}/profile".format(BASE_URL)
+    headers = {"Cookie": f"session_id={session_id}"}
+    response = requests.get(url, headers=headers)
+    assert response.status_code == 200
+
+
+def log_out(session_id: str) -> None:
+    """testing profi,e log out"""
+    url = "{}/sessions".format(BASE_URL)
+    headers = {"Cookie": "session_id={}".format(session_id)}
+    response = requests.delete(url, headers=headers)
+    assert response.status_code in [200, 302],\
+        "Unexpected status code: {}".format(response.status_code)
+
+
+def reset_password_token(email: str) -> str:
+    """Requests a password reset and returns the reset token."""
+    url = f"{BASE_URL}/reset_password"
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+
+    assert response.status_code in [200, 500]
+    assert "email" in response.json()
+    assert response.json()["email"] == email
+    assert "reset_token" in response.json()
+
+    return response.json().get('reset_token')
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """testing update password"""
+    url = "{}/reset_password".format(BASE_URL)
+    data = {"email": email, "reset_token": reset_token,
+            "new_password": new_password}
+    response = requests.put(url, data=data)
+    assert response.status_code == 200
+
+
+if __name__ == "__main__":
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
