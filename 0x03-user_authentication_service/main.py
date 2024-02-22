0476133bde2cc1c0b8926f2b2dd7cf10c6e560e7
Thapelo-ST <tsamkelo96@gmail.com>
2024-02-22 23:13:59 +0200

    db fix 2

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,89 +2,32 @@
 """
 Main file
 """
+
 from db import DB
 from user import User
 
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-
 my_db = DB()
 
-# Test Case 1: Valid user
-user = my_db.add_user("test@test.com", "PwdHashed")
-print(user.id)
-print("\n user Added!! \n")
+# Test Case 1: Adding a valid user
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+assert user_1.id is not None, "User ID should not be None"
+print("\nTest Case 1: User added successfully - ID: \n", user_1.id)
 
-find_user = my_db.find_user_by(email="test@test.com")
-print(find_user.id)
-print("\n Test case {} done !! \n".format(1))
+# Test Case 2: Adding another valid user
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+assert user_2.id is not None, "User ID should not be None"
+print("\nTest Case 2: Another user added successfully - ID: \n", user_2.id)
 
-# Test Case 2: User not found
+# Test Case 3: Adding a user with an empty email (should raise IntegrityError)
 try:
-    find_user = my_db.find_user_by(email="test2@test.com")
-    print(find_user.id)
-except NoResultFound:
-    print("Not found")
-print("\n Test case {} done !! \n".format(2))
+    invalid_user = my_db.add_user("", "InvalidHashedPwd")
+    print("\nTest Case 3: Unexpected - Empty email should raise IntegrityError\n")
+except Exception as e:
+    print("\nTest Case 3: Expected -\n", e)
 
-# Test Case 3: Invalid query argument
+# Test Case 4: Adding a user with an empty password (should raise IntegrityError)
 try:
-    find_user = my_db.find_user_by(no_email="test@test.com")
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid")
-print("\n Test case {} done !! \n".format(3))
-
-# Additional Test Cases:
-
-# Test Case 4: Multiple valid users with the same email (should raise InvalidRequestError)
-my_db.add_user("duplicate@test.com", "PwdHashed1")
-my_db.add_user("duplicate@test.com", "PwdHashed2")
-try:
-    find_user = my_db.find_user_by(email="duplicate@test.com")
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid")
-print("\n Test case {} done !! \n".format(4))
-
-# Test Case 5: Invalid query with additional arguments (should raise InvalidRequestError)
-try:
-    find_user = my_db.find_user_by(email="test@test.com", invalid_arg="test")
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid")
-print("\n Test case {} done !! \n".format(5))
-
-# Test Case 6: Invalid query with no arguments
-try:
-    find_user = my_db.find_user_by()
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid")
-print("\n Test case {} done !! \n".format(6))
-
-# Test Case 7: Invalid query with unknown attribute
-try:
-    find_user = my_db.find_user_by(invalid_attr="test")
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid")
-print("\n Test case {} done !! \n".format(7))
-
-# Test Case 8: User not found with valid query
-try:
-    find_user = my_db.find_user_by(email="nonexistent@test.com")
-    print(find_user.id)
-except NoResultFound:
-    print("Not found")
-print("\n Test case {} done !! \n".format(8))
-
-# Test Case 9: User not found with invalid query attribute
-try:
-    find_user = my_db.find_user_by(invalid_attr="test")
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid query attribute")
-except NoResultFound:
-    print("Not found")
-print("\n Test case {} done !! \n".format(9))
+    invalid_user = my_db.add_user("invalid@test.com", "")
+    print("\nTest Case 4: Unexpected - Empty password should raise IntegrityError\n")
+except Exception as e:
+    print("\nTest Case 4: Expected -\n", e)
